95e1c3fc0e2c29e822aa12263b50042172f58dcf
NatrezC <cnatrez@gmail.com>
2020-10-20 14:34:43 -0500

    finished project

--- Controllers/prehistoric_creatures.js
+++ Controllers/prehistoric_creatures.js
@@ -33,6 +33,17 @@ router.get('/:idx', (req, res)=>{
     res.render('prehistoric_creatures/show', {creature: creatureData[creatureIndex], creatureId: creatureIndex})
 })
 
+// ----> precreatures POST ROUTE <------
+router.post('/', (req, res)=>{
+    let creatures = fs.readFileSync('./prehistoric_creatures.json')
+    let creatureData = JSON.parse(creatures)
+    creatureData.push(req.body) // push the new dino to the array
+    // save the new dinoData array to the dinosaurs.json file
+    // JSON.stringify does the opposite of JSON.parse
+    fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatureData))
+    // redirect to the GET /dinosaurs route (index)
+    res.redirect('/prehistoric_creatures')
+})
 
 
 module.exports =  router;
